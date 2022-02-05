48e9fe4433cef8c427e19d32b1de4f02bfdc5a68
esgaelramos <esgaelramos@gmail.com>
2022-02-05 08:07:49 -0600

    Create urls app and change urls with include, create views as Class

--- myapi/views.py
+++ myapi/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+#Existen VIEWS about CLASS and about FUNCTIONS;
+
+class CompanyView(view):
+    
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
